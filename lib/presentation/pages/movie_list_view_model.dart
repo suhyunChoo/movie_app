514cbb3814c19e386f00bafcf78d26e3c10b6419
suhyunChoo <s_where@naver.com>
2024-12-24 20:44:08 +0900

    add: new project

---
+++ lib/presentation/pages/movie_list_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MovieListViewModel extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
